9850e9ffadff059bf38edd7282ae10c5fff6c29d
Lee Hochan <leehochan@Leeui-MacBookPro.local>
2024-03-31 21:29:54 +0900

    to do list part 2: use map to dynamically create a list

--- src/App.js
+++ src/App.js
@@ -12,14 +12,17 @@ function App() {
     setToDos(currentArray => [toDo, ...currentArray]);
     setToDo("");
   }
-  console.log(toDos);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
+      <h1>My To Dos({toDos.length})</h1>
       <form onSubmit={onSubmit}>
       <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
       <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
   );
 }
