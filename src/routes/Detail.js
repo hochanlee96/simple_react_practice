32f9b1943bb7fb68c74a7143bf6207ca085165b4
Lee Hochan <leehochan@Leeui-MacBookPro.local>
2024-04-01 01:36:55 +0900

    finish the router part

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setMovie(json.data.movie);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  console.log(movie);
+  return (
+    <div>
+      <div>
+        <img src={""} alt="" />
+        <p>
+          {
+            "Sit exercitation proident adipisicing dolore veniam cupidatat incididunt ea tempor esse est deserunt ullamco id."
+          }
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
