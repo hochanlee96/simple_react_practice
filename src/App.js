2d939e1fb362e92badc92e8635cd6c670b84303f
Lee Hochan <leehochan@Leeui-MacBookPro.local>
2024-03-31 21:59:38 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {useState} from "react";
 import Todo from "./Todo.js";
+import CoinTracker from "./CoinTracker.js";
 
 function App() {
   return (
     <div>
-      <Todo />
+      {/* <Todo /> */}
+      <CoinTracker />
     </div>
   );
 }
