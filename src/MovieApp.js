658fe7114e730f792896ac93e2b81286d91bc178
Lee Hochan <leehochan@Leeui-MacBookPro.local>
2024-03-31 23:17:26 +0900

    using async await instead of then

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -3,16 +3,15 @@ import { useState, useEffect } from "react";
 function MovieApp() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
-  }, []);
+
+  const getMovies = async () => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json");
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {}, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
