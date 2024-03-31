2e974888f086b7b85485d71aed905277fb562704
Lee Hochan <leehochan@Leeui-MacBookPro.local>
2024-04-01 00:33:54 +0900

    use link to avoid refreshing the page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="" />
       <h2>
-        <a href="/movie">{title}</a>
+        <Link to="/movie">{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
